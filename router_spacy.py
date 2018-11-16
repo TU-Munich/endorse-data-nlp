1f4fc93859404d885d5ef7f236255080532631d5
Sebastian Erhardt <sebastian@ex-studios.net>
2018-11-16 15:43:41 +0100

    +/- split functions for spacy tasks + save it in database

--- router_spacy.py
+++ router_spacy.py
@@ -1,18 +1,22 @@
 from flask import Flask, Blueprint, request, jsonify
 import spacy
+import time
+from elasticsearch import Elasticsearch
+import uuid
+
+es = Elasticsearch()
 
 nlp = spacy.load('en_core_web_sm')
 
 api_spacy = Blueprint('api_spacy', __name__, template_folder='templates')
 
 
-@api_spacy.route("/parse/")
-def parse_text():
-    sentence = request.args.get('sentence')
-    doc = nlp(sentence)
-    result = {}
-    result["input"] = sentence
-
+def doc_pos(doc):
+    """
+    Take a spacy doc object
+    extract all necessary information
+    from the part of speech subsection
+    """
     pos = []
     for token in doc:
         pos.append({
@@ -25,19 +29,56 @@ def parse_text():
             "is_alpha": token.is_alpha,
             "is_stop": token.is_stop
         })
+    return pos
 
+
+def doc_ner(doc):
+    """
+    Take a spacy doc object
+    extract all necessary information
+    from the named entity subsection
+    """
     ner = []
     for ent in doc.ents:
         ner.append(
             {
-                "text": ent.text, "start": ent.start_char, "end": ent.end_char, "label": ent.label_
+                "text": ent.text,
+                "start": ent.start_char,
+                "end": ent.end_char,
+                "label": ent.label_
             })
+    return ner
 
-    result['pos'] = pos
-    result['ner'] = ner
 
+def handle_sentence(sentence, debug=False):
+    # spacy classify document
+    doc = nlp(sentence)
+    # init result dict
+    result = dict()
+    # add input sentence
+    result["input"] = sentence
+    # add unix timestamp
+    result["timestamp"] = time.time(),
+    # Part of Speech tagging
+    result['pos'] = doc_pos(doc)
+    # Named entity recognition
+    result['ner'] = doc_ner(doc)
+    # Save in database if its not debugged
+    if debug:
+        print(result)
+        res = result
+    else:
+        res = es.index(index="test-index", doc_type='sentence', id=uuid.uuid1(), body=result)
+
+    return res
+
+
+@api_spacy.route("/parse/")
+def parse_text():
+    # parse arguments
+    debug = request.args.get('debug', default=False)
+    sentence = request.args.get('sentence')
+    # handle sentence
+    result = handle_sentence(sentence, debug)
+    # return json result
     return jsonify(result)
-
-    @api_spacy.route("/")
-    def index_page():
-        return "This is a website about burritos"
