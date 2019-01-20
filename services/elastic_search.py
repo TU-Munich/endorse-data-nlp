17f5068d5621787c672f2a5b235c03baa422ca32
Marco Antonio Salazar González <marco.salazar93@gmail.com>
2019-01-20 21:20:48 +0100

    Initialise ES using env variables.

--- services/elastic_search.py
+++ services/elastic_search.py
@@ -6,12 +6,10 @@ ES_HOST = os.environ.get('ELASTIC_SERACH_HOST', None)
 ES_USERNAME = os.environ.get('ELASTIC_SERACH_USERNAME', None)
 ES_PASSWORD = os.environ.get('ELASTIC_SERACH_PASSWORD', None)
 
-es = Elasticsearch()
+# es = Elasticsearch()
 
-# es = Elasticsearch(
-#     [ES_HOST],
-#     http_auth=(ES_USERNAME, ES_PASSWORD),
-#     scheme="http",
-#     port=80,
-# )
-#
+es = Elasticsearch(
+    ES_HOST,
+    http_auth=(ES_USERNAME, ES_PASSWORD),
+    scheme="http",
+)
