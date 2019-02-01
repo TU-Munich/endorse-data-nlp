baad123474a85772af56b948f3dbad3e5da8b097
Sebastian Erhardt <sebastian@ex-studios.net>
2019-02-01 23:30:34 +0100

    + continue similarity 2

--- services/similarity/similarity_service.py
+++ services/similarity/similarity_service.py
@@ -1,15 +1,43 @@
 import os
+import numpy as np
 from services.similarity.Encoders import LASEREncoder
+from services.similarity.Encoders import operations
 
 encoder_path = os.environ['LASER'] + "/models/bilstm.93langs.2018-12-26.pt"
 # bpe_codes = os.environ['LASER']+"/models/bilstm.93langs.2018-12-26.pt"
-encoder_laser = LASEREncoder(encoder_path)
+enc = LASEREncoder(encoder_path)
 
 
-def create_document_vector():
-    pass
+def create_document_vector(sentences_vectors, chosen_operations="mean"):
+    """
+    create a documents vector based on sentence vectors
+    :param sentences_vectors:
+    :param chosen_operations:
+    :return:
+    """
+    sentences_vectors_array = []
+    sentences_vectors_array += operations[chosen_operations][0](sentences_vectors)
+    result = np.concatenate(
+        sentences_vectors_array,
+        axis=0
+    )
+    return result
+
+
+def create_sentences_vectors(sentences):
+    """
+    Create a sentence vector
+    :param sentences:
+    :return:
+    """
+    results = enc.encode_sentenceses(sentences)
+    return results
 
 
 def create_sentence_vector(sentence):
-    pass
-
+    """
+    :param sentence:
+    :return:
+    """
+    results = enc.encode_sentence(sentence)
+    return results
