7bcd18ee2e8ef2b1b4ef1684fe2965e68a8abbde
Sebastian Erhardt <sebastian@ex-studios.net>
2019-02-12 22:46:48 +0100

    + create initial projects

--- config/initial.py
+++ config/initial.py
@@ -8,9 +8,27 @@ def init_initial_project():
     :param es:
     :return:
     """
-    # print("[INITIAL] Create initial project")
+    print("[INITIAL] Create initial project")
     test = {
         "name": "Hillary Clinton Emails",
         "date": datetime.datetime.now(),
     }
     es.index(index="projects-index", doc_type='project', id="hillary", body=test)
+
+    test = {
+        "name": "Reuters Mexico Sentiment",
+        "date": datetime.datetime.now(),
+    }
+    es.index(index="projects-index", doc_type='project', id="reuters-mexico", body=test)
+
+    test = {
+        "name": "SEBA Master Lecture",
+        "date": datetime.datetime.now(),
+    }
+    es.index(index="projects-index", doc_type='project', id="seba-master", body=test)
+
+    test = {
+        "name": "Student Similarity",
+        "date": datetime.datetime.now(),
+    }
+    es.index(index="projects-index", doc_type='project', id="student-similarity", body=test)
